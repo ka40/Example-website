41ab0761057e0bc565253aa5544db20ee6cadd39
ka40 <kevbislip@hotmail.com>
2015-08-09 21:22:50 -0400

    se
    se

---
+++ scripts/new 1.js
@@ -0,0 +1,21 @@
+// Write your code below!
+
+var swag = function()
+    {
+        var name = "Kevin";
+
+        while(bool)
+        { 
+        for(i = 1; i < 3; i++){
+            bool = true;
+        };
+        do(){ 
+            console.log("this is number", i, "!")
+        };
+    };
+    if (i = 2) {
+    bool = false;
+    };
+};
+
+swag();
